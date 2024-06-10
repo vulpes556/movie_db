86f5fc42f207d3e466897fa94555e2c4a2d7c626
Bálint <szelasszie@gmail.com>
2024-06-10 17:09:56 +0200

    Co-authored-by: gb4rt0s <gb4rt0s@users.noreply.github.com> to do : list names of actors, writers and directors

--- client/public/script.js
+++ client/public/script.js
@@ -1,17 +1,66 @@
 import { data } from '/data.js';
+const createMovieDiv = movie => {
+  const movieDiv = document.createElement("div") 
+  movieDiv.innerHTML = `<h2>${movie.title}</h2>`
+  return movieDiv
+}
+const createWriterList = movie =>{
+  const writersUl = document.createElement("ul")
+  writersUl.innerHTML = "<h3>Writers:</h3>"
+  movie.writers.forEach(writer => {
+    writersUl.innerHTML += `<li>${data["professionals"][writer-1]["name"]}</li>`
+  });
+  return writersUl
+}
+const createActorList = movie =>{
+  const actorsUl = document.createElement("ul")
+  actorsUl.innerHTML = "<h3>Actors:</h3>"
+  movie.actors.forEach(actor => {
+    actorsUl.innerHTML += `<li>${data["professionals"][actor-1]["name"]}</li>`
+  });
+  return actorsUl
+}
+const createDirectorList = movie =>{
+  const directorsUl = document.createElement("ul")
+   directorsUl.innerHTML = "<h3>Directors:</h3>"
+  movie.directors.forEach(director => {
+    directorsUl.innerHTML += `<li>${data["professionals"][director-1]["name"]}</li>`
+  });
+  return directorsUl
+}
 
-const loadEvent = function() {
+
+const loadEvent = function () {
 
   const page = window.location.pathname.substring(1);
-  
+
   console.log("data: ", data);
   console.log("page: ", page);
-  
-  const rootElement = document.getElementById("root");
 
-  
-  
+  const rootElement = document.getElementById("root");
+  data.movies.forEach(movie =>{
+    const tempMovieDiv = createMovieDiv(movie)
+    if (page === "movies") {
+      tempMovieDiv.append(createWriterList(movie),createDirectorList(movie),createActorList(movie))
+      rootElement.appendChild(tempMovieDiv)
+    }else if(page === "actors"){
+      tempMovieDiv.append(createActorList(movie))
+      rootElement.appendChild(tempMovieDiv)
+    }else if(page === "directors"){
+      tempMovieDiv.append(createDirectorList(movie))
+      rootElement.appendChild(tempMovieDiv)
+    }else if(page === "writers"){
+      tempMovieDiv.append(createWriterList(movie))
+      rootElement.appendChild(tempMovieDiv)
+    }
+
+  })
+
+
+
+
 
 }
 
 window.addEventListener("load", loadEvent);
+
--- process.js
+++ process.js
@@ -11,13 +11,13 @@ const allUniquePers = [];
 let indexCounter = 1
 const sortWriters = (tempMovie) => {
 	const allWriters = tempMovie.writers.map(writer => writer)
-	allWriters.forEach(person =>{
+	allWriters.forEach(person => {
 		let tempObject = {
 			id: 0,
 			name: " ",
 			roles: []
 		}
-		if(!allUniquePers.includes(person)){
+		if (!allUniquePers.includes(person)) {
 			allUniquePers.push(person)
 			tempObject.id = indexCounter;
 			tempObject.name = person
@@ -27,13 +27,13 @@ const sortWriters = (tempMovie) => {
 		}
 
 	})
-	
+
 
 }
 const sortDirectors = (tempMovie) => {
 	const allDirectors = tempMovie.directors.map(director => director)
-	allDirectors.forEach(person =>{
-		if(!allUniquePers.includes(person)){
+	allDirectors.forEach(person => {
+		if (!allUniquePers.includes(person)) {
 			let tempObject = {
 				id: 0,
 				name: " ",
@@ -42,25 +42,25 @@ const sortDirectors = (tempMovie) => {
 			allUniquePers.push(person)
 			tempObject.id = indexCounter;
 			tempObject.name = person
-			tempObject.roles.push("Writer")
+			tempObject.roles.push("Director")
 			movieDB.professionals.push(tempObject)
 			indexCounter++
-		}else{
-			movieDB.professionals.forEach(prof =>{
+		} else {
+			movieDB.professionals.forEach(prof => {
 				if (!prof.roles.includes("Director")) {
-					prof.roles.push("Director")						
+					prof.roles.push("Director")
 				}
-			} )
+			})
 		}
 
 	})
-	
+
 
 }
 const sortActors = (tempMovie) => {
 	const allActors = tempMovie.actors.map(actor => actor)
-	allActors.forEach(person =>{
-		if(!allUniquePers.includes(person)){
+	allActors.forEach(person => {
+		if (!allUniquePers.includes(person)) {
 			let tempObject = {
 				id: 0,
 				name: " ",
@@ -69,25 +69,46 @@ const sortActors = (tempMovie) => {
 			allUniquePers.push(person)
 			tempObject.id = indexCounter;
 			tempObject.name = person
-			tempObject.roles.push("Writer")
+			tempObject.roles.push("Actor")
 			movieDB.professionals.push(tempObject)
 			indexCounter++
-		}else{
-			movieDB.professionals.forEach(prof =>{
+		} else {
+			movieDB.professionals.forEach(prof => {
 				if (prof.name === person) {
 					if (!prof.roles.includes("Actor")) {
-						prof.roles.push("Actor")						
+						prof.roles.push("Actor")
 					}
 				}
-			} )
+			})
 		}
 
 	})
-	
+
 
 }
+const movieIdReplacement = (movie) => {
+	movieDB.professionals.forEach(person => {
+		movie.writers.forEach((writer, index) => {
+			if (person.name === writer) {
+				movie.writers[index] = person.id
+			}
+		})
+		movie.actors.forEach((actor, index) => {
+			if (person.name === actor) {
+				movie.actors[index] = person.id
+			}
+		})
+		movie.directors.forEach((director, index) => {
+			if (person.name === director) {
+				movie.directors[index] = person.id
+			}
+		})
 
-data.movies.forEach(movie =>{
+
+	})
+	return movie
+}
+data.movies.forEach(movie => {
 	sortWriters(movie)
 	sortDirectors(movie)
 	sortActors(movie)
@@ -95,8 +116,25 @@ data.movies.forEach(movie =>{
 })
 fs.writeFileSync("tempData.json", JSON.stringify(movieDB.professionals))
 const allNames = movieDB.professionals.map(person => person.name)
-const uniqueNames = allNames.filter((value,index,array) => array.indexOf(value)===index)
+const uniqueNames = allNames.filter((value, index, array) => array.indexOf(value) === index)
 console.log(allNames.length, uniqueNames.length);
+
+let uniqe_id = 0 + Math.PI
+console.log(Math.PI);
+movieDB.movies = data.movies.map(movie => {
+	movieIdReplacement(movie)
+	movie["uniqe_id"] = uniqe_id
+	uniqe_id += Math.PI
+	return movie
+
+})
+fs.writeFileSync("tempData2.json", JSON.stringify(movieDB.movies))
+
+
+
+
+
+
 
 
 
